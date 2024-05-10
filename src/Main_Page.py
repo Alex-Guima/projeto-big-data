e1c4c9fe981f4dbf1c6ef03230baa204c9366f53
Alex-Guima <alexandreagtaveira@gmail.com>
2024-05-10 12:00:07 -0300

    User uploaded files

--- src/Main_Page.py
+++ src/Main_Page.py
@@ -7,9 +7,11 @@ st.set_page_config(
 
 @st.cache_resource
 def load_data():
-    notas = pd.read_excel("data/TrabalhoBigB1.xlsx", skiprows=[1])
-    notas = notas.round(1)
-    return notas
+    tabela_notas = st.file_uploader("Insira a tabela:", ".xlsx")
+    if tabela_notas is not None:
+        notas = pd.read_excel(tabela_notas, skiprows=[1])
+        notas = notas.round(1)
+        return notas
 
 st.write("# Bem-vindos as suas estatisticas")
 
