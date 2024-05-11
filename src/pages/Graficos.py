5ba6ffe968c84356ed233671bc8c7dc53609eb0e
Alex-Guima <alexandreagtaveira@gmail.com>
2024-05-11 18:11:23 -0300

    Finishing touches on Graficos page

--- src/pages/Graficos.py
+++ src/pages/Graficos.py
@@ -36,31 +36,30 @@ tipos_selecionados = st.multiselect("Escolha a modalidade de avaliacao:",
 tab1, tab2, tab3 = st.tabs(["Turma 1", "Turma 2", "Turma 3"])
 
 with tab1:
-
-    if tipos_selecionados != [] and opcoes_selecionadas != []:
+    if tipos_selecionados == [] and opcoes_selecionadas == []:
+        st.warning("Por favor selecione pelo menos uma materia e um dos tipos de avaliacao")
+    elif len(tipos_selecionados) == 1 and len(opcoes_selecionadas) == 1:
+        st.warning("Por favor selecione mais materias ou mais tipos de avaliacao") 
+    else:
         "## Grafico Geral"
         "---"
         st.line_chart(turma_1.get_dataframe(tipos_selecionados, opcoes_selecionadas).drop(columns="NOME"))
-    elif tipos_selecionados.__len__ == 1 and opcoes_selecionadas.__len__ == 1:
-        st.warning("Por favor selecione pelo mais materias ou mais tipos de avaliacao")
-    else:
-        st.warning("Por favor selecione pelo menos uma materia e um dos tipos de avaliacao")
 with tab2:
-
-    if tipos_selecionados != [] and opcoes_selecionadas != []:
+    if tipos_selecionados == [] and opcoes_selecionadas == []:
+        st.warning("Por favor selecione pelo menos uma materia e um dos tipos de avaliacao")
+    elif len(tipos_selecionados) == 1 and len(opcoes_selecionadas) == 1:
+        st.warning("Por favor selecione mais materias ou mais tipos de avaliacao") 
+    else:
         "## Grafico Geral"
         "---"
         st.line_chart(turma_2.get_dataframe(tipos_selecionados, opcoes_selecionadas).drop(columns="NOME"))
-    elif tipos_selecionados.__len__ == 1 and opcoes_selecionadas.__len__ == 1:
-        st.warning("Por favor selecione pelo mais materias ou mais tipos de avaliacao")
-    else:
-        st.warning("Por favor selecione pelo menos uma materia e um dos tipos de avaliacao")
+        
 with tab3:
-    if tipos_selecionados != [] and opcoes_selecionadas != []:
+    if tipos_selecionados == [] and opcoes_selecionadas == []:
+        st.warning("Por favor selecione pelo menos uma materia e um dos tipos de avaliacao")
+    elif len(tipos_selecionados) == 1 and len(opcoes_selecionadas) == 1:
+        st.warning("Por favor selecione mais materias ou mais tipos de avaliacao") 
+    else:
         "## Grafico Geral"
         "---"
         st.line_chart(turma_3.get_dataframe(tipos_selecionados, opcoes_selecionadas).drop(columns="NOME"))
-    elif tipos_selecionados.__len__ == 1 and opcoes_selecionadas.__len__ == 1:
-        st.warning("Por favor selecione pelo mais materias ou mais tipos de avaliacao")
-    else:
-        st.warning("Por favor selecione pelo menos uma materia e um dos tipos de avaliacao")
