a006a6151a11d15838273c23c97e720e7db85913
Alex-Guima <alexandreagtaveira@gmail.com>
2024-05-13 12:05:16 -0300

    Minor fixes on Graficos Page

--- src/Main_Page.py
+++ src/Main_Page.py
@@ -10,7 +10,10 @@ st.write("# Bem-vindos as suas estatisticas")
 st.write("## Upload de Arquivos")
 st.info("Caso deseje, faca upload de tabelas aqui")
 
-uploaded_file = st.file_uploader("Selecione seu Arquivo", [".xlsx", ".xls"])
+def file_uploader_callback():
+    st.session_state['notas'] = uploaded_file
+
+uploaded_file = st.file_uploader("Selecione seu Arquivo", [".xlsx", ".xls"], on_change=form_callback())
 
 if 'uploaded_file' not in st.session_state:
     st.session_state['uploaded_file'] = None
