8c81d59505a7898dbb104b850c135e647d124a4c
Alex-Guima <alexandreagtaveira@gmail.com>
2024-05-15 19:19:07 -0300

    Mostra percentual de alunos abaixo e acima da media em Resultados

--- src/pages/Resultado.py
+++ src/pages/Resultado.py
@@ -21,13 +21,23 @@ opcoes_selecionadas = st.multiselect("Escolha as materias que deseja ver:",
 
 st.write ("# Media - Primeiro Bimestre")
 
-nome = st.text_input("Insira o nome do Aluno (de acordo com a tabela)")
+turma = Turma()
 
-if nome:
-    turma = Turma()
-    dataframe = turma.get_dataframe_by_materia(opcoes_selecionadas)
-    notas_aluno_especifico = turma.filter_nota_by_aluno(nome, dataframe)
-    notas_aluno_especifico
-else:
-    st.warning("Por favor insira um nome")
-        
+dataframe = turma.get_dataframe_by_materia(opcoes_selecionadas)
+dataframe = dataframe.drop(columns="NOME")
+for opcao in opcoes_selecionadas:
+    opcao_extensao = opcao + ".M"
+    abaixo_ou_acima_dataframe = pd.DataFrame()
+    acima = dataframe[dataframe[opcao_extensao] > 6].count()
+    abaixo = dataframe[dataframe[opcao_extensao] < 6].count()
+    abaixo_ou_acima_dataframe.insert(0, "Acima", acima)
+    abaixo_ou_acima_dataframe.insert(1, "Abaixo", abaixo)
+    abaixo_ou_acima_dataframe = abaixo_ou_acima_dataframe.transpose()
+    fig = px.pie(abaixo_ou_acima_dataframe,
+    values=opcao_extensao,
+    names=["Acima", "Abaixo"],
+    color_discrete_sequence=px.colors.sequential.Bluered,
+    hole=.98,
+    labels=['Acima', 'Abaixo']
+    )
+    st.plotly_chart(fig)
